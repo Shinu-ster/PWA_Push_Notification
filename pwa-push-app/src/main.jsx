8a70b6b95637f1fe9dcd50524add6b086be4ecf4
Shinu-ster <sajatbazz@gmail.com>
2025-02-20 14:52:11 +0545

    Edited Frontend

--- pwa-push-app/src/main.jsx
+++ pwa-push-app/src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Workbox } from 'workbox-window';
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('/service-worker.js');
+  wb.register();
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
